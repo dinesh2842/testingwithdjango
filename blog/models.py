ca7d5cf352e5c718aef3de0b14ba5c1eb9150fe9
dinesh <dineshtech0208@gmail.com>
2025-06-13 15:57:24 +0530

    'testing.yml_created'

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -7,3 +8,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
